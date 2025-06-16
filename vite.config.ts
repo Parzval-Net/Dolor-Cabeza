0e9018a1057c5f233bd780cdbe47fbbac232bb4b
Nicolas <113535204+Parzval-Net@users.noreply.github.com>
2025-06-15 20:59:33 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/Dolor-Cabeza/" : "/",
+  base: mode === "production" ? "/dolor-cabeza/" : "/",
   server: {
     host: "::",
     port: 8080,
